0272cf56e9263f0834323d09ea74f86abb0a3cc7
Henrik Santos <henrik.ruan4@gmail.com>
2023-05-08 00:44:13 -0300

    feat: UI logic

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,19 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public Player player;
+
+    public TextMeshProUGUI scoreText;
+
+    public GameObject playButton;
+
+    public GameObject gameOverButton;
+
     private int score;
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        player.enabled = false;
+    }
+
+    private void Awake()
+    {
+        Time.timeScale = 0f;
+        Application.targetFrameRate = 60;
+
+        Pause();
+    }
+
+    public void Play()
+    {
+        score = 0;
+        scoreText.text = score.ToString();
+
+        gameOverButton.SetActive(false);
+        playButton.SetActive(false);
+
+        Time.timeScale = 1f;
+
+        player.enabled=true;
+
+        Pipes[] pipes = FindObjectsOfType<Pipes>();
+        foreach (var item in pipes)
+        {
+            Destroy(item.gameObject);
+        }
+    }
 
     public void gameOver()
     {
-        Debug.Log("Game Over");
+        gameOverButton.SetActive(true);
+        playButton.SetActive(true);
+
+        Pause();
     }
 
     public void increaseScore()
     {
         score++;
-        Debug.Log(score);
+        scoreText.text = score.ToString();
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -34,6 +34,14 @@ public class Player : MonoBehaviour
         InvokeRepeating(nameof(AnimateSprite), 0.15f, 0.15f);
     }
 
+    private void OnEnable()
+    {
+        Vector3 position = transform.position;
+        position.y = 0f;
+        transform.position = position;
+        direction = Vector3.zero;
+    }
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
